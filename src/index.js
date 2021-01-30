c02c881b059c8b4e8b0bb327adc1b525f9f88cbf
Akari Hatakeyama <htk@AkarinoMacBook-Pro.local>
2021-01-31 00:58:04 +0900

    add light, ground and shadow

--- src/index.js
+++ src/index.js
@@ -4,19 +4,44 @@ let scene, camera, renderer, box;
 
 function init() {
     scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xa0a0a0);
+    scene.fog = new THREE.Fog(0xa0a0a0, 8, 18);
 
-    camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 1.0, 10.0);
-    camera.position.x = -0.3;
-    camera.position.y = 1.7;
-    camera.position.z = 8.2;
-    camera.rotation.x = -12.0 * (Math.PI / 180);
+    camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 1.0, 100.0);
+    camera.position.set(1, 3, 8);
+    camera.lookAt(0, 1, 0);
 
     renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.shadowMap.enabled = true;
+
+    const hemiLight = new THREE.HemisphereLight(0xffffff, 0x333333);
+    hemiLight.position.set(0, 30, 0);
+    scene.add(hemiLight);
+
+    const dirLight = new THREE.DirectionalLight();
+    dirLight.position.set(5, 5, 5);
+    dirLight.castShadow = true;
+    dirLight.shadow.camera.top = 2;
+    dirLight.shadow.camera.bottom = -2;
+    dirLight.shadow.camera.left = -2;
+    dirLight.shadow.camera.right = 2;
+    dirLight.shadow.camera.near = 0.1;
+    dirLight.shadow.camera.far = 40;
+    scene.add(dirLight);
+
+    const groundGeometry = new THREE.PlaneGeometry(100, 100);
+    const groundMaterial = new THREE.MeshStandardMaterial();
+    const ground = new THREE.Mesh(groundGeometry, groundMaterial);
+    ground.rotation.x = -Math.PI / 2;
+    ground.receiveShadow = true;
+    scene.add(ground);
 
     const geometry = new THREE.BoxGeometry();
-    const material = new THREE.MeshBasicMaterial();
+    const material = new THREE.MeshStandardMaterial({ color: 0xaaaaaa });
     box = new THREE.Mesh(geometry, material);
+    box.castShadow = true;
+    box.position.set(0, 1, 0);
     scene.add(box);
 
     document.body.appendChild(renderer.domElement);
@@ -28,7 +53,7 @@ function init() {
 
 function animate() {
     renderer.setAnimationLoop(animate);
-    box.rotation.y += 0.05;
+    box.rotation.y += 0.02;
     renderer.render(scene, camera);
 }
 
