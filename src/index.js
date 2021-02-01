cb3fa3bd8e9fbcf8c4e8c16e495666498ca5bb9d
Akari Hatakeyama <htk@AkarinoMacBook-Pro.local>
2021-02-01 15:08:06 +0900

    refactoring

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { loadAssets } from './lib/assetManager.js';
 
 import assets from './assets.js';
 
-let scene, camera, renderer, controls, box, backRoom;
+let scene, camera, renderer, controls, debugBox, backRoom;
 const debug = true;
 
 function init() {
@@ -25,6 +25,29 @@ function init() {
     pointLight.position.set(20, 25, 12);
     scene.add(pointLight);
 
+    // PCのマウスで視点操作・キーボードで移動可能に
+    addPointerLockControls();
+
+    loadAssets(
+        'assets/',
+        assets,
+        () => {
+            backRoom = assets['back_side_room_model'].scene;
+            backRoom.rotation.x = -Math.PI / 2;
+            scene.add(backRoom);
+
+            debug && addDebugBox();
+
+            document.body.appendChild(renderer.domElement);
+            renderer.setAnimationLoop(animate);
+        },
+        debug
+    );
+
+    window.addEventListener('resize', onResize, false);
+}
+
+function addPointerLockControls() {
     controls = new PointerLockControls(camera, renderer.domElement);
     document.body.addEventListener('click', () => controls.lock());
     document.body.addEventListener('keydown', ev => {
@@ -58,33 +81,22 @@ function init() {
         }
     });
     scene.add(controls.getObject());
+}
 
-    loadAssets(
-        'assets/',
-        assets,
-        () => {
-            backRoom = assets['back_side_room_model'].scene;
-            backRoom.rotation.x = -Math.PI / 2;
-            scene.add(backRoom);
-
-            box = assets['box_model'].scene.getObjectByName('Cube');
-            box.material = new THREE.MeshLambertMaterial({
-                color: 0x777777,
-            });
-            scene.add(box);
-
-            document.body.appendChild(renderer.domElement);
-            renderer.setAnimationLoop(animate);
-        },
-        debug
-    );
-
-    window.addEventListener('resize', onResize, false);
+// 原点・座標軸の把握・material確認等に使用
+function addDebugBox() {
+    debugBox = assets['box_model'].scene.getObjectByName('Cube');
+    debugBox.material = new THREE.MeshLambertMaterial({
+        color: 0x777777,
+    });
+    scene.add(debugBox);
 }
 
 function animate() {
     renderer.setAnimationLoop(animate);
-    box.rotation.y += 0.02;
+    if (debug) {
+        debugBox.rotation.y += 0.02;
+    }
     renderer.render(scene, camera);
 }
 
