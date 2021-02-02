7a68d4fc8f83c460480ecb3af4e0ee922b3cdf40
Akari Hatakeyama <htk@AkarinoMacBook-Pro.local>
2021-02-02 12:01:59 +0900

    add signage, balls and bulbLight

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import * as THREE from 'three';
 import { PointerLockControls } from 'three/examples/jsm/controls/PointerLockControls.js';
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js';
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js';
+import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass.js';
 
 import { loadAssets } from './lib/assetManager.js';
 
 import assets from './assets.js';
 
 let scene, camera, renderer, controls, debugBox, backRoom;
-const debug = true;
+let signage1, signage2, signage3, ball1, ball2, ball3, bloomComposer, bulbLight;
+
+// true時はconsoleにassets読み込みのmessage表示、テスト用のboxオブジェクト表示
+const debug = false;
 
 function init() {
     scene = new THREE.Scene();
@@ -15,15 +21,15 @@ function init() {
     camera.position.set(10, 20, 3);
     camera.lookAt(25, 17, 13);
 
-    renderer = new THREE.WebGLRenderer();
+    renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.shadowMap.enabled = true;
     renderer.outputEncoding = THREE.sRGBEncoding;
     renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.shadowMap.enabled = true;
+    renderer.toneMapping = THREE.ReinhardToneMapping;
+    renderer.toneMappingExposure = Math.pow(0.8, 1); // to allow for very bright scenes.
 
-    const pointLight = new THREE.PointLight();
-    pointLight.position.set(20, 25, 12);
-    scene.add(pointLight);
+    scene.add(new THREE.AmbientLight(0x404040));
 
     // PCのマウスで視点操作・キーボードで移動可能に
     addPointerLockControls();
@@ -44,7 +50,83 @@ function init() {
         debug
     );
 
+    // outer glow effect
+    const renderScene = new RenderPass(scene, camera);
+    const bloomPass = new UnrealBloomPass(
+        new THREE.Vector2(window.innerWidth, window.innerHeight),
+        1.3,
+        0.0,
+        0.3
+    );
+
+    bloomComposer = new EffectComposer(renderer);
+    bloomComposer.addPass(renderScene);
+    bloomComposer.addPass(bloomPass);
+
+    bloomComposer.render();
+
+    addBulbLight();
+    addSignage();
+    addBalls();
+
     window.addEventListener('resize', onResize, false);
+}
+
+function addBulbLight() {
+    bulbLight = new THREE.PointLight(0xffee88, 1, 30, 3);
+    const bulbGeometry = new THREE.SphereGeometry(0.3, 8, 8);
+    const bulbMaterial = new THREE.MeshStandardMaterial({
+        emissive: 0xffffee,
+        emissiveIntensity: 10,
+    });
+    bulbLight.add(new THREE.Mesh(bulbGeometry, bulbMaterial));
+    bulbLight.position.y = 5;
+    bulbLight.castShadow = true;
+    scene.add(bulbLight);
+}
+
+function addSignage() {
+    const signageGeometryWide = new THREE.PlaneGeometry(11, 6, 1, 1);
+    const signageGeometry = new THREE.PlaneGeometry(7, 6, 1, 1);
+    const signageMaterial = new THREE.MeshBasicMaterial({
+        color: 0xa0a0a0,
+        side: THREE.FrontSide,
+    });
+    const xPosition = 41;
+
+    signage1 = new THREE.Mesh(signageGeometryWide, signageMaterial);
+    signage1.position.set(xPosition, 23, 6);
+    signage1.rotation.y = -Math.PI / 2;
+    scene.add(signage1);
+
+    signage2 = new THREE.Mesh(signageGeometry, signageMaterial);
+    signage2.position.set(xPosition, 15, 1);
+    signage2.rotation.y = -Math.PI / 2;
+    scene.add(signage2);
+
+    signage3 = new THREE.Mesh(signageGeometry, signageMaterial);
+    signage3.position.set(xPosition, 15, 11);
+    signage3.rotation.y = -Math.PI / 2;
+    scene.add(signage3);
+}
+
+function addBalls() {
+    const ballGeometry = new THREE.SphereGeometry(1, 12, 12);
+    const ballMaterial = new THREE.MeshBasicMaterial({
+        wireframe: true,
+    });
+
+    ball1 = new THREE.Mesh(ballGeometry, ballMaterial);
+    ball1.position.set(25, 17, 17);
+    scene.add(ball1);
+
+    ball2 = new THREE.Mesh(ballGeometry, ballMaterial);
+    ball2.position.set(20, 14, 17);
+    scene.add(ball2);
+
+    ball3 = new THREE.Mesh(ballGeometry, ballMaterial);
+    ball3.position.set(15, 17, 17);
+    scene.add(ball3);
 }
 
 function addPointerLockControls() {
@@ -89,15 +171,27 @@ function addDebugBox() {
     debugBox.material = new THREE.MeshLambertMaterial({
         color: 0x777777,
     });
+    debugBox.position.set(30, 2, 10);
     scene.add(debugBox);
 }
 
 function animate() {
     renderer.setAnimationLoop(animate);
+    const time = Date.now() * 0.001;
+    bulbLight.position.x = Math.sin(time) * 10 + 23;
+    bulbLight.position.z = Math.cos(time) * 7 + 8;
+
+    ball1.rotation.x += 0.01;
+    ball2.rotation.y += 0.01;
+    ball3.rotation.z += 0.01;
+
     if (debug) {
         debugBox.rotation.y += 0.02;
+        debugBox.position.y = 10;
+        debugBox.position.x = Math.sin(time * 2) * 7 + 20;
+        debugBox.position.z = Math.cos(time * 2) * 6 + 8;
     }
-    renderer.render(scene, camera);
+    bloomComposer.render();
 }
 
 function onResize() {
